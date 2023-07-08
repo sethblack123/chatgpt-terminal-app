317e0533b7445be5ab99f286956a8e1a3bc1c2a2
sethblack123 <40411401+sethblack123@users.noreply.github.com>
2023-07-07 19:37:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+import openai from "./open_ai.js"
+import readlineSync from 'readline-sync';
+import colors from 'colors';
+
+
+
+async function main(){
+const chatHistory=[];
+
+
+while (true){
+try{
+  const userInput = readlineSync.question(colors.red("you: "))
+const messages = chatHistory.map(([role,content])=> ({role, content}))
+messages.push({role:"user", content: userInput})
+
+
+
+const completion = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: messages,
+});
+
+//get response
+const completionText = completion.data.choices[0].message.content;
+
+if(userInput.toLocaleLowerCase()==='exit'){
+    console.log(colors.green(`chat bot: ${completionText}`))
+    return;
+} else {
+    console.log(colors.green(`chat bot: ${completionText}`))
+    chatHistory.push(["user", userInput])
+    chatHistory.push(["assistant", completionText])
+}
+}
+catch(e){
+    console.log(colors.red(e))}
+}
+}
+
+main()
